92f1ab5155d8a25b5e8f328819ff25897cbdc8fa
Nicolas Charette Naud <nicolas.charette-naud@beslogic.com>
2021-11-04 21:22:16 -0400

    refactor: Park the authentication for now

--- Troupon.DealManagement.Api/Startup.cs
+++ Troupon.DealManagement.Api/Startup.cs
@@ -35,18 +35,17 @@ namespace Troupon.DealManagement.Api
 
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddOAuthGenericAuthentication(Configuration).AddOAuthM2MAuthFlow();
-
       services.AddControllers().AddNewtonsoftJson();
-      services.AddOAuthController();
 
-      services.AddAuthorization(options =>
-      {
-        options.AddPolicy(TenantPolicy.Key, pb => pb.AddTenantPolicy("pwc"));
-        options.AddPolicy(AdminOnlyPolicy.Key, pb => pb.AddAdminOnlyPolicy());
-      });
-
-      services.AddPolicyHandlers();
+      // AUTH IS PARKED
+      // services.AddOAuthController();
+      // services.AddOAuthGenericAuthentication(Configuration).AddOAuthM2MAuthFlow();
+      // services.AddAuthorization(options =>
+      // {
+      //   options.AddPolicy(TenantPolicy.Key, pb => pb.AddTenantPolicy("pwc"));
+      //   options.AddPolicy(AdminOnlyPolicy.Key, pb => pb.AddAdminOnlyPolicy());
+      // });
+      // services.AddPolicyHandlers();
 
       services.AddAutoMapper(typeof(AutomapperProfile).Assembly, typeof(AutomapperProfileDomain).Assembly);
 
@@ -87,8 +86,10 @@ namespace Troupon.DealManagement.Api
 
       app.UseRouting();
 
-      app.UseAuthentication();
-      app.UseAuthorization();
+      // AUTH IS PARKED
+      // app.UseAuthentication();
+      // app.UseAuthorization();
+
       app.UseEndpoints(endpoints =>
       {
         endpoints.MapControllers();
